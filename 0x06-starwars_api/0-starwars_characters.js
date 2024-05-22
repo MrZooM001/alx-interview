0e8aa44010670fce29fb6b3cb6dd3f58d5bb1c67
Hazem Magdy <hhazem0010@gmail.com>
2024-05-22 23:47:03 +0300

    fix7 output - star wars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,31 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const movieId = process.argv[2];
-const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 if (!movieId) {
-  console.error('Enter the movie ID');
+  console.error('Please provide a Movie ID');
   process.exit(1);
 }
 
+const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 request(filmUrl, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
     return;
   }
+
   if (response.statusCode !== 200) {
     console.error('Failed to fetch film data');
     return;
   }
+
   const filmData = JSON.parse(body);
   const characterUrls = filmData.characters;
-  characterUrls.forEach(url => {
+  const characters = new Array(characterUrls.length);
+  let CompletedRequests = 0;
+
+  characterUrls.forEach((url, index) => {
     request(url, (charError, charResponse, charBody) => {
       if (charError) {
         console.error('Error:', charError);
@@ -32,7 +36,11 @@ request(filmUrl, (error, response, body) => {
         return;
       }
       const characterData = JSON.parse(charBody);
-      console.log(characterData.name);
+      characters[index] = characterData.name;
+      CompletedRequests++;
+      if (CompletedRequests === characterUrls.length) {
+        characters.forEach(character => console.log(character));
+      }
     });
   });
 });
