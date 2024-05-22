ed9db9af414fcd7b2b17730561006b34cd20d45c
Hazem Magdy <hhazem0010@gmail.com>
2024-05-22 23:38:25 +0300

    fix6 star wars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,41 +1,36 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 const request = require('request');
 
 const movieId = process.argv[2];
+const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 if (!movieId) {
-  console.error('Please provide a Movie ID');
+  console.error('Enter the movie ID');
   process.exit(1);
 }
-const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 request(filmUrl, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
     return;
   }
-
   if (response.statusCode !== 200) {
     console.error('Failed to fetch film data');
     return;
   }
-
   const filmData = JSON.parse(body);
   const characterUrls = filmData.characters;
-
   characterUrls.forEach(url => {
     request(url, (charError, charResponse, charBody) => {
       if (charError) {
         console.error('Error:', charError);
         return;
       }
-
       if (charResponse.statusCode !== 200) {
         console.error('Failed to fetch character data');
         return;
       }
-
       const characterData = JSON.parse(charBody);
       console.log(characterData.name);
     });
