99146229370566b3ec44110025e1c0bbd90ac312
Hazem Magdy <hhazem0010@gmail.com>
2024-05-22 23:34:13 +0300

    fix4 star wars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,38 +1,37 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const request = require('request');
+const argv = process.argv;
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
+const filmUrl = `${apiUrl}${argv[2]}/`;
 
-const movieId = process.argv[2];
-const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-if (!movieId) {
-  console.error('Enter the movie ID');
-  process.exit(1);
-}
-
-request(filmUrl, (error, response, body) => {
+request(filmUrl, function (error, response, body) {
   if (error) {
-    console.error('Error:', error);
+    console.error('Failed to fetch movie data:', error);
     return;
   }
-  if (response.statusCode !== 200) {
-    console.error('Failed to fetch film data');
-    return;
+
+  const movieData = JSON.parse(body);
+  const characters = movieData.characters;
+  if (characters && characters.length > 0) {
+    fetchCharacters(characters, 0);
+  } else {
+    console.log('No characters found for this movie.');
   }
-  const filmData = JSON.parse(body);
-  const characterUrls = filmData.characters;
-  characterUrls.forEach(url => {
-    request(url, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error:', charError);
-        return;
-      }
-      if (charResponse.statusCode !== 200) {
-        console.error('Failed to fetch character data');
-        return;
-      }
-      const characterData = JSON.parse(charBody);
-      console.log(characterData.name);
-    });
-  });
 });
+
+function fetchCharacters (characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
+
+  request(characters[index], function (error, response, body) {
+    if (error) {
+      console.error('Failed to fetch character data:', error);
+    } else {
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    }
+    fetchCharacters(characters, index + 1);
+  });
+}
